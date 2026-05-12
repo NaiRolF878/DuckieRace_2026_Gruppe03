6f8ee55b14f2460d0b9d22bfce9c2fc924e0a6b3
Florian Fischer <271710917+NaiRolF878@users.noreply.github.com>
2026-05-12 22:32:44 +0200

    Add ducks.sh script for Duckiebot control

---
+++ launchers/ducks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source devel/setup.bash
+
+trap "kill 0" EXIT
+
+# --- core lane system (immer aktiv, damit der Duckiebot fährt) ---
+rosrun follow_lane detect_lane_node.py &
+rosrun follow_lane switch_control_node.py &
+
+sleep 3
+
+rosrun follow_lane control_lane_node.py &
+
+# --- duck detection (perception) ---
+rosrun ducks detect_duck_node.py &
+
+sleep 2
+
+# --- obstacle / reaction layer ---
+rosrun ducks control_obstacle_node.py &
+
+wait
