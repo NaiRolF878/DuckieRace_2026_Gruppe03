4ec7fb06f155e81ca53d6b9018163016ca438e45
Florian Fischer <271710917+NaiRolF878@users.noreply.github.com>
2026-05-15 10:02:56 +0200

    Add camera_dashboard_node to follow_lane script

--- launchers/follow_lane.sh
+++ launchers/follow_lane.sh
@@ -4,6 +4,7 @@ source devel/setup.bash
 
 rosrun follow_lane detect_lane_node.py &
 rosrun follow_lane switch_control_node.py &
+rosrun follow_lane camera_dashboard_node.py &
 sleep 5
 
 rosrun follow_lane control_lane_node.py &
