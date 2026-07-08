712d5563ce18f4fd34e0cf76854e31bf4ec2e96a
Florian Fischer <271710917+NaiRolF878@users.noreply.github.com>
2026-07-08 23:19:01 +0200

    Update ROS package references in intersection_handling.sh

--- launchers/intersection_handling.sh
+++ launchers/intersection_handling.sh
@@ -2,14 +2,14 @@
 source /opt/ros/noetic/setup.bash
 source devel/setup.bash
 
-rosrun follow_lane detect_lane_node.py &
-rosrun follow_lane detect_sign_node.py &
-#rosrun follow_lane configuration_node.py &
-rosrun follow_lane switch_control_node.py &
-rosrun follow_lane cross_intersection_node.py &
-#rosrun follow_lane dashboard_node.py &
+rosrun intersection_handling detect_lane_node.py &
+rosrun intersection_handling detect_sign_node.py &
+#rosrun intersection_handling configuration_node.py &
+rosrun intersection_handling switch_control_node.py &
+rosrun intersection_handling cross_intersection_node.py &
+#rosrun intersection_handling camera_dashboard_node.py &
 sleep 5
 
-rosrun follow_lane control_lane_node.py
+rosrun intersection_handling control_lane_node.py
 
 wait
